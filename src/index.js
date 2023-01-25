90bc90022fa43aae2fb8ec6e4c70eb679ffd3321
Habtamu210 <beleyaha@gmail.com>
2023-01-25 15:17:14 -0800

    Update entire changes

--- src/index.js
+++ src/index.js
@@ -1,64 +1,6 @@
 import './style.css';
-import Icon from './image/icon.png';
-import arrow from './image/arrow.png';
-import more from './image/more.png';
+import fass from './module/loop.js';
+import checker from './module/check.js';
 
-const icon1 = document.querySelector('#icon');
-icon1.src = Icon;
-const arrowimg = document.querySelector('#arrowimg');
-arrowimg.src = arrow;
-
-const todoList = document.querySelector('#todo-list');
-const taskLists = [
-  {
-    description: 'To Do List App',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'well come to Javascript',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Read more',
-    completed: false,
-    index: 2,
-  },
-];
-
-const display = () => {
-  taskLists.forEach((todo) => {
-    const todoItem = document.createElement('div');
-    todoItem.classList.add('todo-item');
-
-    const label = document.createElement('div');
-    const input = document.createElement('input');
-    const span = document.createElement('span');
-    const content = document.createElement('div');
-    const actions = document.createElement('div');
-    const editImg = document.createElement('img');
-
-    input.type = 'checkbox';
-    input.checked = todo.completed;
-    input.setAttribute('aria-label', 'checkbox');
-    editImg.src = more;
-    editImg.setAttribute('alt', 'restart todo');
-
-    label.classList.add('label');
-    span.classList.add('bubble');
-    content.classList.add('todo-content');
-    actions.classList.add('actions');
-
-    content.innerHTML = `<input type='text' value='${todo.description}' aria-label='task' readonly>`;
-
-    todoList.appendChild(todoItem);
-    todoItem.appendChild(label);
-    todoItem.appendChild(content);
-    todoItem.appendChild(actions);
-    actions.appendChild(editImg);
-    label.appendChild(input);
-    label.appendChild(span);
-  });
-};
-display();
+fass();
+checker();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-unresolved */
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  entry: {
+    bundle: path.resolve(__dirname, './src/index.js'),
+  },
   devServer: {
     static: './dist',
   },
@@ -19,19 +21,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
